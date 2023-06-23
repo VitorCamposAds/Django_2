f681efb0b1ff2a2da1cbeb5a003cc39d60aa7c5a
Vitor <vitorbeatle@gmail.com>
2023-06-23 14:14:33 -0300

    Projeto Finalizado!

---
+++ core/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect
+from .forms import ContatoForm, ProdutoModelForm
+from .models import Produto
+
+def index(request):
+    context = {
+        'produtos': Produto.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def contato(request):
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        print(f'Post: {request.POST}')
+        if form.is_valid():
+            form.send_email()
+            messages.success(request, 'E-mail enviado com sucesso!')
+            form = ContatoForm()
+        else:
+            messages.error(request, 'Erro ao enviar o e-mail!')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
+
+def produto(request):
+    #print(f'Usuário: {request.user}') - teste para printar quem está logado
+    if str(request.user) != 'AnonymousUser':
+        if str(request.method) == "POST":
+            form = ProdutoModelForm(request.POST, request.FILES)
+            if form.is_valid():
+
+                form.save()
+
+                messages.success(request, 'Produto salco com sucesso!')
+                form = ProdutoModelForm()
+            else:
+                messages.error(request, 'Erro ao salvar produto!')
+        else:
+            form = ProdutoModelForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'produto.html', context)
+    else:
+        return redirect('index')
+
+
+
